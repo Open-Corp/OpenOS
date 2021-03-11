3f12990cd5224a52131a5e7a514e57438a52a1ee
Kelbazz <49045228+Kelbazz@users.noreply.github.com>
2021-03-12 00:28:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const version = "0.0.1z";
+
 function menu(menuName) {
   const menuBar = document.querySelector(".section2");
   if (menuName === "changelog") {
@@ -11,12 +13,22 @@ function menu(menuName) {
   }
   if (menuName === "about") {
     menuBar.innerHTML = `
-    <h1>About.</h1>
+    <h1>A propos d'Open OS...</h1>
+    <hr>
+    <p>Open OS est un systeme d'exploitation (en cours de creation) disponible sur le web ! Il vous suffit d'un navigateur pour le faire fonctionner.</p>
     `
   }
   if (menuName === "use") {
     menuBar.innerHTML = `
-    <h1>Use.</h1>
+    <h1>Comment l'utiliser ?</h1>
+    <hr>
+    <p>Pour utiliser Open OS, c'est un petit peu plus complexe que de simplement cliquer sur une icone !</p>
+    <h2>1.</h2>
+    <p>Télécharger le fichier zip.<br>(<= C'est juste ici !)</p>
+    <h2>2.</h2>
+    <p>Allez retrouver le fichier Téléchargé sur votre ordinateur et décompressez le.</p>
+    <h2>3.</h2>
+    <p>Ouvrer le dossier "OpenOS" et cliquer sur "index.html"</p>
     `
   }
   menuBar.innerHTML += `
@@ -28,3 +40,6 @@ function menu(menuName) {
   `
 }
 menu('changelog');
+
+const dlBtn = document.querySelector(".dl-btn");
+dlBtn.href = `os-dl/OpenOS-${version}.zip`;
